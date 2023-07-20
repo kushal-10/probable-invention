aa9522ba3dd11a689338c9ea6cec908b0d903891
kushal-10 <kushalkoshti11@gmail.com>
2023-07-20 14:48:17 +0200

    update max_tokens

--- captions.py
+++ captions.py
@@ -33,7 +33,7 @@ def generate_captions():
     inputs = processor(image, return_tensors="pt").to(device, torch.float16)
     # inputs = processor(image, return_tensors="pt").to(device)
 
-    generated_ids = model.generate(**inputs, max_new_tokens=20)
+    generated_ids = model.generate(**inputs, max_new_tokens=200)
     print(len(generated_ids))
     generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
     print(generated_text)

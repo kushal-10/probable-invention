371b1947d7fde373dc1f4de39df0713a30e09db9
kushal-10 <kushalkoshti11@gmail.com>
2023-07-20 14:35:00 +0200

    add float 16

--- captions.py
+++ captions.py
@@ -24,14 +24,14 @@ def generate_captions():
     
     # GET CORRESPONDING CAPTIONS
     processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
-    # model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch_dtype=torch.float16)
-    model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b")  
+    model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch_dtype=torch.float16)
+    # model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b")  
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.to(device)
 
     image = Image.open(image_list[0])
-    # inputs = processor(image, return_tensors="pt").to(device, torch.float16)
-    inputs = processor(image, return_tensors="pt").to(device)
+    inputs = processor(image, return_tensors="pt").to(device, torch.float16)
+    # inputs = processor(image, return_tensors="pt").to(device)
 
     generated_ids = model.generate(**inputs, max_new_tokens=20)
     print(len(generated_ids))
